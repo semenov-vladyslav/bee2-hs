107d5d7e0be6a6caaad06dd185137a7ab2a684f7
Vlad Semenov <semenov.vlad.by@gmail.com>
2017-04-01 22:22:56 +0300

    Bign bignSign2, bignVerify

--- src/Bee2/Crypto/Bign.hs
+++ src/Bee2/Crypto/Bign.hs
@@ -1,18 +1,25 @@
 module Bee2.Crypto.Bign
-  ( 
+  ( PrivKey, PubKey, HashValue, HashOid, SigValue
+  , bignSign2'bs, bignVerify'bs
   ) where
 
 import Bee2.Defs
 import Bee2.Foreign
 
+import Data.Word
+  ( Word(..)
+  )
 import Foreign.C.Types
   ( CUInt(..), CSize(..)
   )
 import Foreign.Ptr
-  ( FunPtr
+  ( FunPtr, nullPtr, castPtr
   )
 import Foreign.C.String
   ( withCString
+  )
+import System.IO.Unsafe
+  ( unsafePerformIO
   )
 
 -- bign_params is defined as a struct, but we don't need to directly access it's fields
@@ -88,6 +95,51 @@ bignStd192 :: BignParams
 bignStd192 = bignStdParams bignStd192Oid
 bignStd256 :: BignParams
 bignStd256 = bignStdParams bignStd256Oid
+
+bignStd :: Word -> BignParams
+bignStd 128 = bignStd128
+bignStd 192 = bignStd192
+bignStd 256 = bignStd256
+bignStd _ = error "bignStd invalid l (must be [128,192,256])"
+
+type PrivKey = Octets
+type PubKey = Octets
+type HashValue = Octets
+type HashOid = Octets
+type SigValue = Octets
+
+bignSign2'bs :: Word -> PrivKey -> HashOid -> HashValue -> SigValue
+bignSign2'bs l priv oid hash
+  | l /= 128 && l /= 192 && l /= 256 = error "bignSign2 invalid l (must be [128,192,256]"
+  | getSize priv /= fromIntegral (l `div` 4) = error "bignSign2 invalid priv size (must be [32,48,64])"
+  | getSize hash /= fromIntegral (l `div` 4) = error "bignSign2 invalid hash size (must be [32,48,64])"
+  | otherwise =
+      unsafeCreate' (fromIntegral (3 * l `div` 8)) $ \psig ->
+      unsafeUseAsCStringLen' (bignStd l) $ \pparams sparams ->
+      unsafeUseAsCStringLen' priv $ \ppriv spriv ->
+      unsafeUseAsCStringLen' oid $ \poid soid ->
+      unsafeUseAsCStringLen' hash $ \phash shash ->
+      return $! bignSign2'cptr psig (castPtr pparams) poid (fromIntegral soid) phash ppriv nullPtr 0
+
+bignVerify'bs :: Word -> PubKey -> HashOid -> HashValue -> SigValue -> Bool
+bignVerify'bs l pub oid hash sig
+  | l /= 128 && l /= 192 && l /= 256 = error "bignVerify invalid l (must be [128,192,256]"
+  | getSize pub /= fromIntegral (2 * l `div` 4) = error "bignVerify invalid pub size (must be [64,92,128])"
+  | getSize hash /= fromIntegral (l `div` 4) = error "bignVerify invalid hash size (must be [32,48,64])"
+  | getSize sig /= fromIntegral (3 * l `div` 8) = error "bignVerify invalid sig size (must be [48,72,96])"
+  | otherwise = unsafePerformIO $ 
+      unsafeUseAsCStringLen' (bignStd l) $ \pparams sparams ->
+      unsafeUseAsCStringLen' pub $ \ppub spub ->
+      unsafeUseAsCStringLen' oid $ \poid soid ->
+      unsafeUseAsCStringLen' hash $ \phash shash ->
+      unsafeUseAsCStringLen' sig $ \psig ssig -> do
+      let e = bignVerify'cptr (castPtr pparams) poid (fromIntegral soid) phash psig ppub
+      -- 0 ok, 408 - bad_sig
+      case e of
+        0 -> return True
+        408 -> return False
+        _ -> error $ "bignVerify failed: error " ++ show e
+
 
 {-
 
